08ba116e953c95c7b6e0073610afa55ed1b77e75
Left Arm <jrm.kaewsiri@gmail.com>
2022-05-18 09:54:06 +0700

    add admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $admin = new User();
+        $admin->name = 'lannacom';
+        $admin->email = 'admin@lanna.co.th';
+        $admin->email_verified_at = now();
+        $admin->password = bcrypt('Lannacom@1');
+        $admin->remember_token = Str::random(10);
+        $admin->save();
         \App\Models\User::factory(10)->create();
     }
 }
