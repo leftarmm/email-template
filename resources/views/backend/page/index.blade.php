60f7de9137ce9de7d1cb57dc6959fb7c36833b79
oatchara0612 <jar.tukaew1907@gmail.com>
2022-06-09 09:08:16 +0700

    page2

--- resources/views/backend/page/index.blade.php
+++ resources/views/backend/page/index.blade.php
@@ -1,53 +1,68 @@
 @extends('layouts.backend')
 
 @section('content')
-
-<div class="card">
-    <div class="card-header pb-0">
-        <div class="row">
-            <div class="col-lg-6 col-7">
-                <h6>Page</h6>
+    <div class="card">
+        <div class="card-header pb-0">
+            <div class="row">
+                <div class="col-lg-6 col-7">
+                    <h6>Page</h6>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="card-body">
-<div class="form-group">
-    <label for="">Host</label>
-    <select class="form-control input-sm" name="host" > 
-         @foreach($hosts as $data)
-           <option value="{{$data->id}}">{{$data->url}}</option>
-         @endforeach
-    </select>
-</div>
-</div>
-<div class="card-body">
-<div class="form-group">
-    <label for="">User</label>
-    <select class="form-control input-sm" name="name" > 
-         @foreach($admins as $data)
-           <option value="{{$data->id}}">{{$data->name}}    </option>
-         @endforeach
-    </select>
-</div>
-</div>
-<div class="card-body">
-<div class="form-group">
-    <label for="">Email Template</label>
-    <select class="form-control input-sm" name="body" > 
-         @foreach($templates as $data)
-           <option value="{{$data->id}}">{{$data->body}}   </option>
-         @endforeach
-    </select>
-</div>
-</div>
-<div class="card-body">
-<div class="form-group">
-    <label for="">Logs</label>
-    <select class="form-control input-sm" name="body" > 
-         @foreach($logs as $data)
-           <option value="{{$data->id}}">{{$data->reciever}} {{$data->status}} {{$data->detail}}   </option>
-         @endforeach
-    </select>
-</div>
-</div>
-@endsection
+        <div class="card-body">
+            <div class="form-group">
+                <label for="">Host</label>
+                <select class="form-control input-sm" name="host">
+                    @foreach ($hosts as $data)
+                        <option value="{{ $data->id }}">{{ $data->url }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="">User</label>
+                <select class="form-control input-sm" name="name">
+                    @foreach ($admins as $data)
+                        <option value="{{ $data->id }}">{{ $data->name }} </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="">Email Template</label>
+                <select class="form-control input-sm" name="body">
+                    @foreach ($templates as $data)
+                        <option value="{{ $data->id }}">{{ $data->body }} </option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <label for="">Logs</label>
+                <table class="table align-items-center mb-0">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Reciever</th>
+                            <th>Status</th>
+                            <th>Detail</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($logs as $item)
+                            <tr>
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->reciever }}</td>
+                                <td>{{ $item->status }}</td>
+                                <td>{{ $item->detail }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endsection
+    
